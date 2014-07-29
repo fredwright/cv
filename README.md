33ae9672e455de60e49220b03a33f1ff0b6ab0e6
fredwright <fredwright0@gmail.com>
2014-07-29 22:42:21 +0100

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+CV
+==========
+
+personal resume using:
+
+	stylus
+	jade
+	gulp
+
+clone and run on port 9000 using 'gulp' command
---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+
+//======================================================
+// OVERVIEW
+//======================================================
+
+// gulp compiles sass and jade, watches for changes
+// and serves with livereload... simple
+
+//======================================================
+// SETUP
+//======================================================
+
+// gulp
+var gulp = require('gulp'); 
+
+// plugins
+var jade = require('gulp-jade'),
+    stylus = require('gulp-stylus'),
+    connect = require('gulp-connect');
+
+// paths
+var app = './app',
+    dist = './dist',
+    css = '/css';
+
+//======================================================
+// PROCESS
+//======================================================
+
+// compile stylus
+gulp.task('stylus', function() {
+    gulp.src(app + '/*.styl')
+        .pipe(stylus())
+        .pipe(gulp.dest(dist + css))
+        .pipe(connect.reload());
+});
+
+// compile jade
+gulp.task('jade', function() {
+    gulp.src(app + '/*.jade')
+        .pipe(jade())
+        .pipe(gulp.dest(dist))
+        .pipe(connect.reload());
+});
+
+// move fonts
+gulp.task('fonts', function () {
+    gulp.src(app + css + '/fonts/*')
+        .pipe(gulp.dest(dist + css))
+        .pipe(connect.reload());
+});
+
+//======================================================
+// SERVE
+//======================================================
+
+// watch
+gulp.task('watch', function() {
+    gulp.watch(app + '/*.jade', ['jade']);
+    gulp.watch(app + css + '/*.styl', ['stylus']);
+    gulp.watch(app + css + '/fonts/*', ['fonts']);
+});
+
+// serve
+gulp.task('connect', function() {
+  connect.server({
+    root: dist,
+    port: 9000,
+    livereload: true
+  });
+});
+
+// default
+gulp.task('default', [ 'jade', 'stylus', 'fonts', 'watch', 'connect' ]);
